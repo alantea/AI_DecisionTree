1022c8af797ef07266c577332f5979d4b3099125
alantea <alan094@gmail.com>
2014-12-20 20:01:56 +0800

    Fix typesetting & add Makefile

--- decision_tree.h
+++ decision_tree.h
@@ -1,20 +1,28 @@
+#ifndef DECISON_TREE_H_INCLUDED
+#define DECISON_TREE_H_INCLUDED
+
+// C library
+#include <cmath>
+
+// C++ library
 #include <fstream>
-#include <math.h>
+
+// define value
 #define MAX_SAMPLE 700
 
 using namespace std;
 
-#ifndef DECISON_TREE_H_INCLUDED
-#define DECISON_TREE_H_INCLUDED
-
 class decision_tree
-{private:  int entropy(int sequence[25][2]);
-           int attribute[MAX_SAMPLE][25];
-           int choseroad[25][6];
-  public: void read_file();
-          void gain_tree(bool invaild[MAX_SAMPLE],int sequence[25][2],int root=24);
-          void save_tree();
-           decision_tree();
+{
+	private:
+		int entropy(int sequence[25][2]);
+        int attribute[MAX_SAMPLE][25];
+        int choseroad[25][6];
+	public:
+        decision_tree();
+		void read_file();
+        void gain_tree(bool invaild[MAX_SAMPLE],int sequence[25][2],int root=24);
+        void save_tree();
 };
 
 #endif // DECISON_TREE_H_INCLUDED
--- main.cpp
+++ main.cpp
@@ -1,20 +1,25 @@
-#include "decison_tree.h"
+#include "decision_tree.h"
 
 using namespace std;
 
 int main()
-{ // The first part : produce the decision tree.
-     decision_tree goal;
-     bool invaild[MAX_SAMPLE] = {0};
-     int sequence[25][2];
-     for(int i=0;i<25;i++)
-        {sequence[i][0]=25;
-         sequence[i][1]=25;
-        }
-     goal.read_file();
-     goal.gain_tree(invaild,sequence);
-     goal.save_tree();
-  // The second part : test the data.
+{
+	// The first part : produce the decision tree.
+	decision_tree goal;
+	bool invaild[MAX_SAMPLE] = {0};
+	int sequence[25][2];
+	
+	for(int i=0;i<25;i++)
+	{
+		sequence[i][0]=25;
+		sequence[i][1]=25;
+	}
+	
+	goal.read_file();
+	goal.gain_tree(invaild,sequence);
+	goal.save_tree();
+	
+	// The second part : test the data.
 
- return 0;
+	return 0;
 }
